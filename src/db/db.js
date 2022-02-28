5c23d450396d3886db2b3a0c314dbfee6129b532
root <root-_-@2400-1A00-B1C0-ip6.wlink.com.np>
2022-03-01 03:31:47 +0545

    backend db setup

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
-// mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oahmr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useUnifiedTopology: true,
-//         useFindAndModify: false
-//     })
-    mongoose.connect('mongodb://localhost:27017/ecommerce',{
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oahmr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
-        useCreateIndex:true,
+        useCreateIndex: true,
         useUnifiedTopology: true,
         useFindAndModify: false
     })
+    // mongoose.connect('mongodb://localhost:27017/ecommerce',{
+    //     useNewUrlParser: true,
+    //     useCreateIndex:true,
+    //     useUnifiedTopology: true,
+    //     useFindAndModify: false
+    // })
     .then(() => {
         console.log("connected");
     }).catch((err) => {
