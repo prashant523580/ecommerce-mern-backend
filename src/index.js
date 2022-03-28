f441abb6533f81ebb3fa065e5237a1d1117d1c4d
prashant523580 <prashant.thapa62758@gmail.com>
2022-03-28 21:52:33 +0545

    added body parser

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     next()
 })
 app.use(cookieParser());
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 app.use("/public/", express.static(path.join(__dirname, "uploads")));
