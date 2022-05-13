048d7c2bb4838fede476315f85205bd4baf5bc42
prashant523580 <prashant.thapa62758@gmail.com>
2022-05-13 23:17:46 +0545

    uploaded images

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const adminOrderRoute = require("./routes/admin/admin.order");
 app.use(cors());
 app.use((req, res, next) => {
     
-    res.header("Cross-Origin-Resource-Policy", "Same-Origin")
+//    res.header("Cross-Origin-Resource-Policy", "Same-Origin")
     res.header("Access-Control-Allow-Origin", "*")
     
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD,OPTIONS, PUT, PATCH, DELETE")
